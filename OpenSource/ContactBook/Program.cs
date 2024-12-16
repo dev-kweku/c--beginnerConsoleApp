039abb244c02ea4f59bda4501e8a53e3413038b3
dev-kweku <frikasoidfrimpong@gmail.com>
2024-12-16 14:02:56 +0000

    update added

--- OpenSource/ContactBook/Program.cs
+++ OpenSource/ContactBook/Program.cs
@@ -23,7 +23,7 @@ public ContactBook{
         Console.WriteLine("Contact added successfully")
     }
 
-    public void SearchContent(string name){
+    public void SearchContact(string name){
         bool found=false;
         foreach(Contact contact in contacts){
             if(contact.Name.Equals(name,StringComparison.OrdinalIgnoreCase)){
@@ -62,8 +62,59 @@ public ContactBook{
             }
         }
         if(!found){
-            Console.WriteLine("Contact not found ")
+            Console.WriteLine("Contact not found ");
         }
     }
 
+}
+
+class Program{
+    static void Main(string[] args){
+        ContactBook contactBook=new ContactBook();
+        bool exist=false;
+
+        while(!exit){
+            Console.WriteLine("Welcome to the Contact !!!!!!");
+            Console.WriteLine("1.Add a contact");
+            Console.WriteLine("2. Search for contact");
+            Console.WriteLine("3. Update a contact");
+            Console.WriteLine("4. Exit from contact book");
+
+            Console.Write("Enter your option: ");
+            int option=int.Parse(Console.ReadLine())
+
+            // switch implementation
+            switch(option){
+                case 1:
+                    Console.Write("Enter name: ");
+                    string name=Console.ReadLine();
+                    Console.Write("Enter Email :");
+                    string email=Console.ReadLine();
+                    Console.Write("Enter phone number :");
+                    string phone=Console.ReadLine();
+
+                    Contact contact=new Contact(name,email,phone);
+                    contactBook.AddContact(contact);
+                    break;
+
+                case 2:
+                    Console.Write("Enter name to search: ");
+                    string searchName=Console.ReadLine();
+                    contactBook.SearchContact(searchName);
+                    break;
+                case 3:
+                    Console.Write("Enter name to search: ");
+                    string updateName=Console.ReadLine();
+                    contactBook.UpdateContact(updateName);
+                    break;
+                case 4:
+                    exit=true;
+                    break;
+                default:
+                Console.WriteLine("Invalid option, Try again. :");
+                break;
+
+            }
+        }
+    }
 }
